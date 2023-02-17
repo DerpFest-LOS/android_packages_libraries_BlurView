09f6415723970d28559034a6e52a37d1e3159a9d
HeroBuxx <me@buxxed.tk>
2023-02-17 19:14:05 +0000

    BlurView: Trim repository and keep needed libraries
    Co-authored-by: Lucchetto <lucchetto.tie@live.com>
    Signed-off-by: HeroBuxx <me@buxxed.tk>

--- app/src/main/java/com/eightbitlab/blurview_sample/ImageFragment.java
+++
@@ -1,8 +0,0 @@
-package com.eightbitlab.blurview_sample;
-
-public class ImageFragment extends BaseFragment {
-    @Override
-    int getLayoutId() {
-        return R.layout.fragment_image;
-    }
-}
--- app/src/main/java/com/eightbitlab/blurview_sample/ScrollFragment.java
+++
@@ -1,8 +0,0 @@
-package com.eightbitlab.blurview_sample;
-
-public class ScrollFragment extends BaseFragment {
-    @Override
-    int getLayoutId() {
-        return R.layout.fragment_scroll;
-    }
-}
